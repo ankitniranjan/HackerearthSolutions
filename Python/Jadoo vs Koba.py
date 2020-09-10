98f03f61ea7aa42e38cb43d2b42c54dc4a3e4139
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-09-10 18:44:07 +0530

    Create Jadoo vs Koba.py

---
+++ Python/Jadoo vs Koba.py
@@ -0,0 +1,10 @@
+#		Jadoo, the space alien has challenged Koba to a friendly duel. He asks Koba to write a program to print out all numbers from 70 to 80. Knowing perfectly well how easy the problem is, the Jadoo adds his own twist to the challenge. He asks Koba to write the program without using a single number in his program and also restricts the character limit to 100.
+
+
+# Write your code here
+
+for i in range(ord('F'),ord('Q')):
+    print(i)
+	
+
+#		The ord() function in Python accepts a string of length 1 as an argument and returns the unicode code point representation of the passed argument. For example ord('B') returns 66 which is a unicode code point value of character 'B'.
